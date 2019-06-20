ce63ce813879a08cb02e8f3afd128df3f3c00add
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-20 18:36:21 +0000

    Write a program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Entry point
+*lower - variable that will hold the lowercase character
+*tolower - makes an uppercase chr to go lowercase chr
+*for - makes the loop till the condition become false
+*putchar - to print
+*Return: Always 0 (Success)
+*Using betty style
+*/
+int main(void)
+{
+char x;
+for (x = 'A'; x <= 'Z'; x++)
+{
+int lower = tolower(x);
+putchar (lower);
+}
+return (0);
+}
+
