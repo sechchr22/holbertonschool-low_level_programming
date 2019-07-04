ef581c34c89c9db577da96975228c08b8091e1fd
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-04 20:10:46 +0000

    Write a function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+/**
+*strcpy - function to copy a string
+*@dest: name of the first pointer
+*@src: name of the second pointer
+*@n: number that will tell me till what position of src i want to copy
+*Return: a pointer that points to the value of
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	y = 0;
+
+	while (src[y] != '\0')
+	{
+		y++;
+	}
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	if (x >= y)
+	{
+		dest[x] = '\0';
+	}
+
+return (dest);
+}
