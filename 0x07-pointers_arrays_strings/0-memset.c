f9fe96157a099fc9da70db8b946e31c57b230ea1
sechchr22 <980@holbertonschool.com>
2019-07-08 21:31:53 +0000

    Write a function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 /**
-*memset - function that fills the first n bytes of the memory area pointed to by s with the constant byte b
-*@s: name of the pointer 
+*_memset - fills the first n bytes of the memory with the constant byte b
+*@s: name of the pointer
 *@b: character
-*@n: number 
+*@n: number
 *Return: Pointer to the resultant string s
 */
 char *_memset(char *s, char b, unsigned int n)
@@ -11,9 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (x = 0; x < n/**&& s[x] != '\0'*/; x++)
 	{
-		/** depronto es que 0x00 corresponde a algo que dirige al null y por eso dejando esa condicion en el for se sale apenas ve el primero*/
-		/** la solucion seria con un if que anulara esto pero aun no se */
 		s[x] = b;
 	}
-return (s); 
+return (s);
 }
