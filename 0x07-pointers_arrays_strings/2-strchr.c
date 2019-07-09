43add69da920ada0a864ce6dd17b0d83fa1c3326
sechchr22 <980@holbertonschool.com>
2019-07-09 04:48:56 +0000

    Write a function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,32 @@
 /**
-*
+*_strchr - function to write a function that locates a character in a string
+*@s: name pointer
+*@c: character to share
+*Return: pointer to the resultant string s
 */
 char *_strchr(char *s, char c)
 {
 	int x;
+	int k;
+	int y;
+
+	k = 0;
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
+
 		if (s[x] == c)
 		{
-			for (y = x; y >= 0; y--)
+			for (y = x; s[y] != '\0'; y++)
 			{
-				s[y-x] = s[y];
+				s[k] = s[y];
+				k++;
 			}
-	
-			s[0] = s[x]
-			s[1] = s[x+1]
-			hasta la posicion 0 
+		}
+
+		if (s[x + 1] == s[x])
+		break;
+	}
+return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,9 @@
+/**
+*_strspn - function that gets the length of a prefix substring
+*@s: name of the pointer
+*@accept: name of the second pointer
+*Return: the number of bytes in the initial segment of s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+}
