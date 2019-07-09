5f4e68420fc78ba1557afd996db3e5511219457a
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-09 18:55:32 +0000

    Write a function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,31 +2,17 @@
 *_strchr - function to write a function that locates a character in a string
 *@s: name pointer
 *@c: character to share
-*Return: pointer to the resultant string s
+*Return: pointer to the resultant string s depending on the condition
 */
 char *_strchr(char *s, char c)
 {
 	int x;
-	int k;
-	int y;
-
-	k = 0;
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
 
 		if (s[x] == c)
-		{
-			for (y = x; s[y] != '\0'; y++)
-			{
-				s[k] = s[y];
-				k++;
-			}
-		}
-
-		if (s[x + 1] == s[x])
-		break;
+		return (s + x);
 	}
-return (s);
+return ('\0');
 }
-
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,4 @@
-/**
+wq/**
 *_strspn - function that gets the length of a prefix substring
 *@s: name of the pointer
 *@accept: name of the second pointer
@@ -6,4 +6,24 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
+	int x;
+	int y;
+	unsigned int k;
+
+	k = 0;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (accept[y] == s[x])
+			{
+				k = k + 1;
+				break;
+			}
+			if (accept[y + 1] == '\0')
+			return (k);
+		}
+	}
+return (k);
 }
