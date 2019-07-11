44a5516ced1796799bac5a52b0b38ca38a28d41a
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-11 20:39:30 +0000

    Write a function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,42 @@
+
 /**
-*
-*
+*findsquare - function to iterate and find a square of a number
+*@a: root to find
+*@b: number that i want to find the root
+*Return: a if root exist, -1 if not or imaginary
 */
-int findsquare (int a, int b)
+
+int findsquare(int a, int b)
 {
-	if (a*a == b)
+	if (a * a == b)
 	{
 		return (a);
 	}
 
-	else
+	else if (a > b || b < 0)
 	{
-		a++;
+		return (-1);
 	}
 
-	findsquare(a);
+	a++;
+	return (findsquare(a, b));
 }
+
+/**
+*_sqrt_recursion - function to return the natural root of a number
+*@n : number that i want to find the root
+*Return: 0 if n = 0, returns of the function findsqare otherwise
+*/
 
 int _sqrt_recursion(int n)
 {
 	int a = 1;
-	
+
 	if (n == 0)
 	{
 		return (0);
 	}
 
-	else
-	{
-		findsqare(a, n);
-	}
-}		
+	return (findsquare(a, n));
+}
+
