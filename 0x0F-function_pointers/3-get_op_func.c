0013c920f0259f1c9c56d9e077ef14228d3a7014
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-24 23:25:54 +0000

    Function to call the right function in task 3 - Write a program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include <string.h>
+/**
+ * get_op_func - returns a pointer to the function that
+ * corresponds to the operator given as a parameter
+ * @s: pointer to a char
+ * Return: a pointer to a function corresponding to the s
+ * or 0
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (i < 6)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		return (ops[i].f);
+
+		i++;
+	}
+
+return (0);
+}
+
