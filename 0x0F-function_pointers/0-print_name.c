5513688c05bf70d30378bc99204fb5d0c0338e40
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-24 22:49:21 +0000

    Write a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - function to invoke another function
+ * @name: pointer to a char string
+ * @f: pointer to a function that takes a char pointer
+ * and return nothing
+ * return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
