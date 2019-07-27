faefa359da1b5297c1e652eb030bd97eec0e1157
sechchr22 <980@holbertonschool.com>
2019-07-27 04:38:04 +0000

    function to print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,58 +3,63 @@
 #include <string.h>
 #include <stdarg.h>
 /*
-*
+ *
+ * print_all - function to print "all"
+ * @format: pointer that will recieve valid formats and not
+ * valid formats represented as chars in a string
+ * Return: nothing
 */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
 
 	format_t array[] = {
-	{'c', "%c, ", "%c\n", "va_arg(ap, char)"},
-	{'i', "%i, ", "%i\n", "va_arg(ap, int)"},
-	{'f', "%f, ", "%f\n", "va_arg(ap, float)"},
-	{'s', "%s, ", "%s\n", "va_arg(ap, char*)"},
-	{'0', NULL, NULL, NULL}
+	{'c', "%c, ", "%c\n"},
+	{'i', "%i, ", "%i\n"},
+	{'f', "%f, ", "%f\n"},
+	{'s', "%s, ", "%s\n"},
+	{'0', NULL, NULL}
 	};
 
 	int i; /*para recorrer el string que llega a *format*/
 	int j; /*para recorrer la estructura*/
 	int len; /*guardar el largo del string que llega a *format*/
-	/*char *a;*/ /*para guardar el formatspecifier*/
-	/*char *b;*/ /*para guardar el formatspecifier_2*/
-	/*char *c;*/ /*para guardar el argumento*/
+	char *a; /*para guardar el formatspecifier*/
+	char *b; /*para guardar el formatspecifier_2*/
+	/*char *c;*/ /*para guardar va_arg */
 
 	len = strlen(format);
-	
+
 	i = 0;
 	j = 0;
-	
-	va_start(ap, format);	
+
+	va_start(ap, format);
 
 	while (i < len)
 	{
 		while (array[j].character != '0')
-		{				
+		{
 			if (format[i] == array[j].character)
 			{
-				printf("breve\n");
+				a = array[j].fs;
+				b = array[j].fs_2;
+			
+				/*desde aca lo que copiaste*/
+
+				if ((i + 1) == len)
+				{
+					printf(b, va_arg(ap, int));
+					return;
+				}
+
+				printf(a, va_arg(ap, int));
 				j = 0;
 				break;
-				/*a = array[j].fs;
-			
-				b = array[j].fs_2;
-
-				c = array[j].type;
-				
-				if ( (i + 1) == len)
-				printf(b, c);
-				
-				printf(a, c);*/
-			}	
+			}
 		j++;
 		}
-				
+	j = 0;
 	i++;
 	}
 	va_end(ap);
-}		
+}
