1359174b42ae0eb71843a6e01286fb60c87f1252
sechchr22 <980@holbertonschool.com>
2019-08-09 03:45:57 +0000

    header file

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+int _pow_recursion(int x, int y);
+unsigned int _strlen(const char *s);
+#endif
