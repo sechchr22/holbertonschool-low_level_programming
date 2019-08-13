52f04f1d0b911fb09618505cabf1f37d53d01dc7
sechchr22 <980@holbertonschool.com>
2019-08-13 21:48:11 +0000

    Setting the permissions well

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,22 +28,25 @@ int _strlen(char *s)
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, sz, a;
+	int fd, sz;
 
-	a = _strlen(text_content);
+	/*a = _strlen(text_content);*/
 
-	if ((filename == NULL) | (a == 0))
+	if (filename == NULL)
 	return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == (-1))
 	return (-1);
 
-	sz = write(fd, text_content, _strlen(text_content));
-	if (sz == (-1))
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		sz = write(fd, text_content, _strlen(text_content));
+		if (sz == (-1))
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
