8163fca1046bfe221da82ecd78f0c0a1f321217d
sechchr22 <980@holbertonschool.com>
2019-08-14 22:51:56 +0000

    header file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -6,4 +6,7 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
 void copy (const char *file_from, char *file_to);
+void error_98(const char *a);
+void error_99(char *a);
+void error_100(int a);
 #endif
