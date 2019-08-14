82cbd1429d9ea63e05807f76d73ad35603d35bb0
sechchr22 <980@holbertonschool.com>
2019-08-14 01:13:42 +0000

    all to work at home

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,8 +30,6 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, sz;
 
-	/*a = _strlen(text_content);*/
-
 	if (filename == NULL)
 	return (-1);
 
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		write(STDERR_FILENO, "Usage: cp file_from file_to", 28);
+		exit(97);
+	}
+
+	if (av[1] == NULL)
+	exit(98);
+	
+	copy(av[1], av[2]);
+
+	return (0);
+	
+}
+void copy (const char *file_from, char *file_to)  /* esta debe ser recursiva*/
+{
+	int fd, num, fd2;
+	char buf[1024];
+
+	fd = open(file_from, O_RDONLY);
+
+	fd2 = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664); 
+
+	num = read(fd, buf, 1024);
+	num = write(fd2, buf, num);
+
+	while (num == 1024)
+	{
+		num = write(fd2, buf, num);
+	}
+
+	close(fd);
+	close(fd2);
+}
