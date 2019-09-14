635846d0290dddf75f55bbb4952ed1d0e5f521b9
sechchr22 <980@holbertonschool.com>
2019-09-14 17:30:40 +0000

    function to insert node at a given index in a double linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 	return (NULL);
 
+	temp = *h;
 	new->n = n;
 	if (idx == 0)
 	{
@@ -40,15 +41,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 	return (new);
 	}
+	else if (idx == dlistint_len(*h))
+	return (insert_dnodeint_at_end(temp, new));
 
-	temp = *h;
 	for (i = 0; i < idx; i++)
 	temp = temp->next;
-
 	new->next = (temp->prev)->next;
 	(temp->prev)->next = new;
 	new->prev = temp->prev;
 	temp->prev = new;
-
 return (new);
+}
+
+/**
+ * insert_dnodeint_at_end - insert node at the end of a double linked list
+ * @temp: pointer holding head of the list
+ * @newi: pointer holding the new node
+ * Return: Address of the new node
+*/
+
+dlistint_t *insert_dnodeint_at_end(dlistint_t *temp, dlistint_t *newi)
+{
+	while (temp->next != NULL)
+	temp = temp->next;
+
+	newi->prev = (temp->prev)->next;
+	temp->next = newi;
+	newi->next = NULL;
+
+return (newi);
 }
