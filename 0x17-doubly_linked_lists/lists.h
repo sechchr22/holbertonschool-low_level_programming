27f8d78998750e1bf8f340c461059c8a8a7e9d0a
sechchr22 <980@holbertonschool.com>
2019-09-16 03:06:21 +0000

    header file updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -28,5 +28,8 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_just_one(dlistint_t **head, dlistint_t *temp, unsigned int index);
+int delete_first(dlistint_t **head, dlistint_t *temp);
+int delete_last(dlistint_t *temp, unsigned int index);
 #endif
