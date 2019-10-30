ca710e2148352ca0b9e35604d6eb16a7fd776689
sechchr22 <980@holbertonschool.com>
2019-10-30 23:47:32 +0000

    Header updated

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -24,6 +24,8 @@ void switch_n(listint_t **cm, listint_t **un);
 void bubble_sort(int *array, size_t size);
 void switch_a(int *p_a, int *p_b);
 
+void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
 void swap_int(int *a, int *b);
 int lomutos_partition(int *arr, int lo, int hi, size_t size);
