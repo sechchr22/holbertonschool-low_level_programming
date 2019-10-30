96316fa43a5bdab3607f64b1a49d499feb139f72
sechchr22 <980@holbertonschool.com>
2019-10-30 23:42:02 +0000

    bubble sort algo

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -8,20 +8,39 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0;
-	int tmp;
+	size_t i = 0, j = 0;
+	int stop = 0;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		stop = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				switch_a(&array[j], &array[j + 1]);
+				print_array(array, size);
+				stop = 1;
 			}
-			print_array(array, size);
 		}
+		if (stop == 0)
+			break;
 	}
+}
+/**
+ * switch_a - switch array elemtnts
+ *
+ * @p_a: pointer to first position
+ * @p_b: pointer to second position
+ *
+ */
+void switch_a(int *p_a, int *p_b)
+{
+	int temp;
+
+	temp = *p_a;
+	*p_a = *p_b;
+	*p_b = temp;
 }
