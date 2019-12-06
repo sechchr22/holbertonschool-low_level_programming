d631148faee0f01836e1bd2ef8f0ae9b25c64ccd
sechchr22 <980@holbertonschool.com>
2019-12-06 21:45:01 +0000

    updated

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -41,6 +41,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 int _pow_recursion(int base, int exp);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
