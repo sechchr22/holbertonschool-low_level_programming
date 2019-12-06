642be8f5895cdf397e806a184ac620856322f4b1
sechchr22 <980@holbertonschool.com>
2019-12-06 04:22:10 +0000

    fixed

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -7,10 +7,12 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_left_node;
+
 	if (parent == NULL)
 	return (NULL);
 
-	binary_tree_t *new_left_node = binary_tree_node(parent, value);
+	new_left_node = binary_tree_node(parent, value);
 
 	if (parent->left != NULL)
 	{
