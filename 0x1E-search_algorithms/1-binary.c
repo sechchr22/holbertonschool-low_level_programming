278aadcd8550fbf05001435e59cfdfe935e99265
sechchr22 <980@holbertonschool.com>
2020-01-16 04:34:04 +0000

    Header updated and binary search code

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,73 @@
+#include "search_algos.h"
+
+/**
+ * print_array - to print an array
+ * @array: pointer to an array of int
+ * @l: lowest index
+ * @h: highest index
+ * Return: nothing
+*/
+
+void print_array(int *array, unsigned int l, size_t h)
+{
+	unsigned int i;
+
+	printf("Searching in array: ");
+
+	for (i = l; i <= h; i++)
+	{
+		if (i == h)
+			printf("%d\n", array[i]);
+		else
+			printf("%d, ", array[i]);
+	}
+}
+
+/**
+ * recursive - recursive binary search function
+ * @array: pointer to an array of int
+ * @l: lowest index
+ * @h: highest index
+ * @value: value to find
+ * Return: index of value to search or -1 otherwise
+*/
+
+int recursive(int *array, unsigned int l, size_t h, int value)
+{
+	unsigned int m;
+
+	if (h >= l)
+	{
+		m = l + (h - l) / 2;
+
+		if (array[m] == value)
+		{
+			print_array(array, l, h);
+			return (m);
+		}
+
+		if (array[m] > value)
+		{
+			print_array(array, l, h);
+			return (recursive(array, l, (m - 1), value));
+		}
+
+		print_array(array, l, h);
+		return (recursive(array, (m + 1), h, value));
+	}
+
+return (-1);
+}
+
+/**
+ * binary_search - main function to use a bainary search algorithm
+ * @array: pointer to an array of int
+ * @size: size of the array
+ * @value: value to find
+ * Return: index of the value to find
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	return (recursive(array, 0, (size - 1), value));
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+int recursive(int *array, unsigned int l, size_t h, int value);
+void print_array(int *array, unsigned int l, size_t h);
 #endif
